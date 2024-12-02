41120cb144a6c595b7b0a3bfa48a259235576177
samiraza19 <samirazagb@gmail.com>
2024-12-02 19:38:42 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,51 @@
+const arr = [42,true,'Hello, World!',{
+    name:'John',
+    age: 25,
+    isstudent:false,},
+    false,
+    3.14,
+    'JavaScript is fun',
+];
+
+// Q1
+// const stringArray = arr.map((e) => e.toString());
+// console.log(stringArray);
+
+// Q2
+// const arr2= arr.filter((e)=> typeof e==='number');
+//     console.log(arr2);
+
+// Q3
+// arr.forEach((e)=>{
+//     console.log(typeof e);
+// });
+
+// Q4
+
+// let Arr= arr.reduce((sum,e)=>{
+//         return  typeof e==='number'?sum+e:sum;
+        
+                
+// });  
+// console.log(Arr)
+// Q5
+// arr.find((e)=>{
+//     console.log(typeof e==='boolean' );
+// })
+
+// Q6
+// arr.findIndex((e,i)=>{
+//     typeof e==='object'?console.log(i):null  
+// })
+
+// Q7
+// const arr2 = arr.some((e)=> typeof e === 'number');
+// console.log(arr2);
+
+// Q8
+// const arr2 = arr.every((e)=> typeof e==='string'
+// );
+// console.log(arr2);
+
+
+    
